8dd8b181fa4108f12a3046352551677fc579a5ba
mak-spectrum <mak.spectrum@gmail.com>
2014-05-03 20:45:20 +0300

    Test platform.

---
+++ course/app/org/profwell/vacancy/auxiliary/VacancySeletionType.java
@@ -0,0 +1,5 @@
+package org.profwell.vacancy.auxiliary;
+
+public enum VacancySeletionType {
+
+}
---
+++ course/test/org/profwell/test/TestPlatform.java
@@ -0,0 +1,29 @@
+package org.profwell.test;
+
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.inMemoryDatabase;
+import static play.test.Helpers.running;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import play.test.FakeApplication;
+
+public abstract class TestPlatform {
+
+    protected void runTest(TestScenario testRun) {
+        Map<String, String> inMem = inMemoryDatabase();
+        Map<String, String> appConf = new HashMap<>();
+        for (Entry<String, String> e : inMem.entrySet()) {
+            appConf.put(e.getKey(), e.getValue());
+        }
+        appConf.put("application.schedulers.enable", "false");
+        appConf.put("db.default.url", "jdbc:h2:mem:trst-db;DB_CLOSE_DELAY=-1;INIT=CREATE SCHEMA IF NOT EXISTS ccmc");
+
+        final FakeApplication app = fakeApplication(appConf);
+
+        running(app, testRun);
+    }
+
+}
